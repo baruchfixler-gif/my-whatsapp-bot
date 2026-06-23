c23d71ebcde7d096727b1f4964295f975fbe61dd
baruchfixler-gif <baruchfixler@gmail.com>
2026-06-23 22:44:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 const { default: makeWASocket, useMultiFileAuthState } = require('baileys');
-const mongoose = require('mongoose');
-
-// חיבור למונגו
-mongoose.connect(process.env.MONGO_URI).catch(err => console.log(err));
 
 async function startBot() {
-    // שימוש בתיקייה זמנית לזיכרון
-    const { state, saveCreds } = await useMultiFileAuthState('./auth_info');
+    console.log('מנסה להתחבר לווטסאפ...');
+    const { state, saveCreds } = await useMultiFileAuthState('./auth_session');
     
     const sock = makeWASocket({
         auth: state,
-        printQRInTerminal: true // ננסה להדפיס QR רגיל שוב
+        printQRInTerminal: true 
     });
 
     sock.ev.on('creds.update', saveCreds);
 
     sock.ev.on('connection.update', (update) => {
         const { connection, qr } = update;
-        if (qr) {
-            console.log('--- מצאתי QR! ---');
-            console.log(qr); // בגרסאות מסוימות זה מדפיס את הקוד, בגרסאות אחרות צריך לסרוק מהמסך
-        }
-        if (connection === 'open') {
-            console.log('הבוט מחובר!');
-        }
+        if (qr) console.log('QR CODE:', qr);
+        if (connection === 'open') console.log('הבוט מחובר!');
     });
 }
 
